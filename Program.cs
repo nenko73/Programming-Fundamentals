642b006f271a96cbcc293e8ecfc886b66791bb1c
nenko73 <106167405+nenko73@users.noreply.github.com>
2022-05-24 18:57:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01StudetInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	student name 
+            //•	age
+            //•	average grade. 
+            string studentName = Console.ReadLine();
+            string age=Console.ReadLine();
+            string drade=Console.ReadLine();
+            // "Name: {student name}, Age: {student age}, Grade: {student grade}".
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {drade}");
+
+        }
+    }
+}
